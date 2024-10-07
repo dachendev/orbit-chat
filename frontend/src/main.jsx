64737350968fa7460a465e3a9974ddf8b0dd98d7
dachendev <13964975+dachendev@users.noreply.github.com>
2024-10-07 17:50:14 -0400

    add reset styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthUserContextProvider } from './features/auth'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
