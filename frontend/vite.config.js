e859005a400c4399ddbf1769de5ee239c2c43f05
dachendev <13964975+dachendev@users.noreply.github.com>
2024-10-07 19:19:03 -0400

    add aliases for shared and features

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,17 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
+const __dirname = import.meta.dirname
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@features': path.resolve(__dirname, './src/features'),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3001',
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(import.meta.dirname, './src'),
     },
   },
 })
