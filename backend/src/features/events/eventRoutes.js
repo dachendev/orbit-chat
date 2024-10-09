659ac37437fbe664bd07b83ff48c16633a5bf1dc
dachendev <13964975+dachendev@users.noreply.github.com>
2024-10-08 20:17:05 -0400

    events: make sse chainable

--- backend/src/features/events/eventRoutes.js
+++ backend/src/features/events/eventRoutes.js
@@ -5,8 +5,7 @@ eventRoutes.use(sseMiddleware())
 
 eventRoutes.get('/', (request, response) => {
   setInterval(() => {
-    response.sse.data('success!')
-    response.sse.dispatch()
+    response.sse.data('success!').dispatch()
   }, 5000)
 })
 
--- backend/src/features/events/sseMiddleware.js
+++ backend/src/features/events/sseMiddleware.js
@@ -1,5 +1,5 @@
 const sseMiddleware = (options = {}) => {
-  const { keepAliveInterval = 5000 } = options
+  const { keepAliveInterval = 15000 } = options
 
   const headers = {
     'Content-Type': 'text/event-stream',
@@ -9,16 +9,30 @@ const sseMiddleware = (options = {}) => {
   return (request, response, next) => {
     response.writeHead(200, headers)
 
-    const comment = (comment) => response.write(`: ${comment}\n`)
-    const field = (name, value) => response.write(`${name}: ${value}\n`)
+    let buffer = []
+
+    const write = (data) => buffer.push(data) && response.sse
+
+    const comment = (comment) => write(`: ${comment}\n`)
+    const field = (name, value) => write(`${name}: ${value}\n`)
     const event = (type) => field('event', type)
     const data = (data) => field('data', data)
-    const dispatch = () => response.write('\n')
 
-    setInterval(() => {
-      comment('keep-alive')
-      dispatch()
+    const dispatch = () => {
+      if (buffer.length > 0) {
+        response.write(buffer.join('') + '\n')
+        buffer = []
+      }
+      return response.sse
+    }
+
+    const intervalId = setInterval(() => {
+      comment('keep-alive').dispatch()
     }, keepAliveInterval)
+
+    request.on('close', () => {
+      clearInterval(intervalId)
+    })
 
     response.sse = {
       comment,
@@ -27,6 +41,8 @@ const sseMiddleware = (options = {}) => {
       data,
       dispatch,
     }
+
+    response.sse.comment('stream').dispatch()
 
     next()
   }
